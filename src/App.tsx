f74c3ac5a69a40e057360807126df97d61f55d08
unknown <268339@student.pwr.edu.pl>
2024-05-13 23:02:27 +0200

    Add view with books list

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LoginForm from './login-form/LoginForm';
+import DisplayBooksForm from './display-books-form/DisplayBooksForm';
 
 function App() {
   return (
     <>
-      <LoginForm></LoginForm>
+      <DisplayBooksForm></DisplayBooksForm>
     </>
   );
 }
